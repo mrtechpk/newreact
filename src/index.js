199183cfb1a99f5285c1016591f536d782906627
Mr PRASHANT KEDAR <85385281+mrtechpk@users.noreply.github.com>
2022-06-06 20:06:57 +0530

    d

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  
+  <h3> first react programm</h3>,
+
+  document.getElementById("root")
+);
+
+
+
